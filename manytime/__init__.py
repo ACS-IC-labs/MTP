c261ebbe3309b64df75657cd96e0a956e2e9a443
razvanalex <razvansmadu@gmail.com>
2026-02-21 02:04:17 +0200

    refactor: Move CLI functionality to manytime module and update entry point

--- manytime/__init__.py
+++ manytime/__init__.py
@@ -2,6 +2,9 @@
 Many-time module
 """
 
+import sys
+import argparse
+
 from manytime import analysis
 from manytime.interactive import interactive, load_from_result
 
@@ -31,3 +34,46 @@ def load_and_continue(
     then opens an interactive session to continue decryption.
     """
     load_from_result(ciphertexts, load_filename, results_filename)
+
+
+def main() -> None:
+    """
+    Main entry point for CLI program
+    """
+    parser = argparse.ArgumentParser(
+        description="Break One-Time Pad Encryption with key reuse"
+    )
+    parser.add_argument(
+        "file",
+        type=str,
+        help="file containing hexadecimal ciphertexts, delimited by new lines",
+    )
+    parser.add_argument(
+        "-o",
+        type=str,
+        dest="output_file",
+        default="result.json",
+        help="filename to export decryptions to",
+    )
+    parser.add_argument(
+        "-l",
+        "--load",
+        type=str,
+        dest="load_file",
+        default=None,
+        help="filename to load previous results from",
+    )
+    args = parser.parse_args()
+
+    with open(args.file, "r") as f:
+        ciphertexts = [line.rstrip() for line in f]
+
+    try:
+        ciphertexts = list(map(bytearray.fromhex, ciphertexts))
+    except ValueError as error:
+        sys.exit("Invalid hexadecimal: {error}")
+
+    if args.load_file:
+        load_and_continue(ciphertexts, args.load_file, args.output_file)
+    else:
+        many_time_pad_attack(ciphertexts, args.output_file)
